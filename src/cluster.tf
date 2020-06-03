4f227b318cad4dc0f5385b36b923323493235fd7
Khionu Sybiern <dev@khionu.net>
2020-06-02 19:49:38 -0500

    Create initial resources; adjust docs

---
+++ src/cluster.tf
@@ -0,0 +1,17 @@
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_kubernetes_cluster" "main" {
+  name    = "arealmreborn"
+  region  = "ams3"
+  version = "1.17.5-do.0"
+
+  node_pool {
+    name       = "primal"
+    size       = "s-1vcpu-2gb"
+    auto_scale = true
+    min_nodes  = 0
+    max_nodes  = 5
+  }
+}
