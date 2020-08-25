7f54574608213e60d77e125e698b109628c6c6f8
Linus Willner <hello@linuswillner.me>
2020-08-25 18:17:31 +0300

    Fix certificate not being decoded for the CA certificate
    Sourced from https://github.com/hashicorp/terraform-provider-helm/issues/37#issuecomment-359301875 and https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs/resources/kubernetes_cluster#kubernetes-terraform-provider-example.

--- src/cluster.tf
+++ src/cluster.tf
@@ -22,6 +22,6 @@ provider "kubernetes" {
   host                   = digitalocean_kubernetes_cluster.main.kube_config.0.host
   client_certificate     = digitalocean_kubernetes_cluster.main.kube_config.0.client_certificate
   client_key             = digitalocean_kubernetes_cluster.main.kube_config.0.client_key
-  cluster_ca_certificate = digitalocean_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate
+  cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
   load_config_file       = false
 }
