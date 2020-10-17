185b0d0afa61d519cea86ceaaed8a5116ed38b7e
Khionu Sybiern <dev@khionu.net>
2020-10-17 15:34:56 -0500

    Modmail web deployment + service

--- src/modmail.tf
+++ src/modmail.tf
@@ -34,3 +34,53 @@ resource "kubernetes_deployment" "modmail_bot" {
     }
   }
 }
+
+resource "kubernetes_deployment" "modmail_web" {
+  metadata {
+    name      = "modmail-web-deployment"
+    namespace = kubernetes_namespace.modmail_ns.id
+  }
+  spec {
+    selector {
+      match_labels = {
+        ident = "modmail-web"
+        type  = "website"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          ident = "modmail-web"
+          type  = "website"
+        }
+      }
+      spec {
+        container {
+          image = "" # TODO: Get correct image
+          name  = "modmail-web"
+
+          # TODO: Env vars
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "modmail_web_service" {
+    metadata {
+        name = "modmail_web"
+    }
+    spec {
+        selector = {
+            ident = kubernetes_deployment.modmail_web.spec.0.selector.labels["ident"]
+            type = kubernetes_deployment.modmail_web.spec.0.selector.labels["type"]
+        }
+        session_affinity = "ClientIP"
+        port {
+            port        = 8080
+            target_port = 80
+        }
+
+        type = "LoadBalancer"
+    }
+}
